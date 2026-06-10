66db7461cca9242183702d57af7665c396bd9337
oscar <okardon@elon.edu>
2026-06-10 15:57:21 -0400

    starter stuff
    Co-authored-by: yonathanm772 <yonathanm772@users.noreply.github.com>

--- employee_app/employee_app.py
+++ employee_app/employee_app.py
@@ -1,1 +1,15 @@
-# starter 
+def __main__():
+    employees = {"oscarkardon": "password"}
+    login(employees)
+    while True:
+        #menu stuff
+
+def login(employees):
+    print("Please enter your username: ")
+    username = input()
+    if username not in employees.keys:
+        print("User does not exist")
+        create_employee()
+    print("Please enter your password: ")
+    password = input()
+
